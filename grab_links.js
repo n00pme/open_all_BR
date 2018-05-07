86b787a956d7914330a47fcbaf837adbbb951ec5
root <root@localhost.localdomain>
2018-05-07 10:19:47 +0800

    a

--- grab_links.js
+++ grab_links.js
@@ -1,23 +1,7 @@
 console.log('open all BR started');
 
-links=[]
-
-for (var i = 1; i < 11; i++) {
-	link=document.getElementById(i)
-	.getElementsByClassName('t')[0]
-	.getElementsByTagName('a')[0]
-	.href
-    //console.log(link); 
-    links = links.concat(link);
-}
-console.log(links)
-
-
-links_json=JSON.stringify(links);
-//console.log(links_json);
-
 document.ondblclick = function(){
-	chrome.runtime.sendMessage({greeting:links_json}, function(response) {
+	chrome.runtime.sendMessage({greeting:grab_links()}, function(response) {
 		console.log(response.farewell);
 	});
 	add_border()
@@ -33,6 +17,20 @@ function add_border(){
 	}
 }
 
+
+function grab_links(){
+	links=[]
+	for (var i = 1; i < 11; i++) {
+		link=document.getElementById(i).getElementsByClassName('t')[0].getElementsByTagName('a')[0].href
+
+		links = links.concat(link);
+	}
+	console.log(links)
+
+	links_json=JSON.stringify(links);
+
+	return links_json;
+}
 
 
 
